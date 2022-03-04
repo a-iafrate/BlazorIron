32388ba29f8bd32792f3d631d7d74899d21f8028
alessio iafrate <alessioiafrate@hotmail.com>
2022-03-04 17:44:10 +0100

    clean code

--- Server/Controllers/WeatherForecastController.cs
+++ Server/Controllers/WeatherForecastController.cs
@@ -10,11 +10,7 @@ namespace BlazorIron.Server.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
+        
         private const int ANGLE_OPEN_MOTOR1 = 160;
         private const int ANGLE_OPEN_MOTOR2 = 20;
         private const int ANGLE_CLOSE_MOTOR1 = 20;
@@ -33,15 +29,8 @@ namespace BlazorIron.Server.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public void Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
         }
 
         [HttpGet("[action]")]
---
+++ Shared/ServoInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorIron.Shared
+{
+    internal class ServoInfo
+    {
+        public int Angle1 { get; set; }
+        public int Angle2 { get; set; }
+    }
+}
